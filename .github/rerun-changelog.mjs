f97e2abfbc6cf1bbf5d8335f67b7646da6832740
Stéphane Brunner <stephane.brunner@camptocamp.com>
2023-09-15 09:27:51 +0200

    Fix rerun changelog

--- .github/rerun-changelog.mjs
+++ .github/rerun-changelog.mjs
@@ -1,20 +1,17 @@
 import { Octokit } from '@octokit/action';
 
-const octokit = new Octokit();
+if (process.env.GITHUB_REF_TYPE == 'tag') {
+    const octokit = new Octokit();
 
-const workflowRuns = await octokit.actions.listWorkflowRuns({
-    owner: 'camptocamp',
-    repo: 'helm-mutualize',
-    workflow_id: 'changelog.yaml',
-    per_page: 1,
-});
-const workflowRun = workflowRuns.data['workflow_runs'][0];
+    const workflowRuns = await octokit.actions.listWorkflowRuns({
+        owner: 'camptocamp',
+        repo: 'helm-mutualize',
+        workflow_id: 'changelog.yaml',
+        per_page: 1,
+    });
+    const workflowRun = workflowRuns.data['workflow_runs'][0];
 
-console.log(workflowRun['status']);
-console.log(workflowRun['conclusion']);
-
-if (workflowRun['status'] != 'completed' || workflowRun['conclusion'] != 'success') {
-    const result = await octokit.actions.reRunWorkflowFailedJobs({
+    const result = await octokit.actions.reRunWorkflow({
         owner: 'camptocamp',
         repo: 'helm-mutualize',
         run_id: workflowRun['id'],
