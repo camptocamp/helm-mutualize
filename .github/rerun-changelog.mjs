a01b1db9a3cec2e9d0d3435860f6d244a6df2f47
Stéphane Brunner <stephane.brunner@camptocamp.com>
2023-09-15 09:45:50 +0200

    Create the changelog after the release creation

--- .github/rerun-changelog.mjs
+++ .github/rerun-changelog.mjs
@@ -3,17 +3,9 @@ import { Octokit } from '@octokit/action';
 if (process.env.GITHUB_REF_TYPE == 'tag') {
     const octokit = new Octokit();
 
-    const workflowRuns = await octokit.actions.listWorkflowRuns({
+    octokit.rest.repos.createDispatchEvent({
         owner: 'camptocamp',
         repo: 'helm-mutualize',
-        workflow_id: 'changelog.yaml',
-        per_page: 1,
-    });
-    const workflowRun = workflowRuns.data['workflow_runs'][0];
-
-    const result = await octokit.actions.reRunWorkflow({
-        owner: 'camptocamp',
-        repo: 'helm-mutualize',
-        run_id: workflowRun['id'],
+        event_type: 'changelog',
     });
 }
